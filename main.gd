a42b62fc86059ebd68a7750ec82bfefef8ee2d8f
Pacihstan <pax@isphere.net>
2023-04-24 16:22:06 -0500

    Created a login screen and subdirectories for furture screens. Added await example due to waiting for a frame being required to properly work with TextEdit nodes in ready.

--- main.gd
+++ main.gd
@@ -3,12 +3,14 @@ var currentData
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$database_interface.configureConnection("Data Source=SILVER;Initial Catalog=BankDB;Integrated Security=True;TrustServerCertificate=True")
+	$database_interface.handleQuery("SELECT * FROM Accounts")
+	$database_interface.closeConnection()
+	await get_tree().process_frame
+	$TextEdit.text += str(currentData)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$database_interface.configureConnection("Data Source=SILVER;Initial Catalog=BankDB;Integrated Security=True;TrustServerCertificate=True")
-	$database_interface.handleQuery("SELECT * FROM Accounts")
-	$database_interface.closeConnection()
-	$TextEdit.text += str(currentData)
+	pass
+	
