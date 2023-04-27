fbc84230f28554a8728d794e8580132c1fd268ed
MujeebAdelekan <56655073+MujeebAdelekan@users.noreply.github.com>
2023-04-27 14:34:23 -0500

    dk

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ var currentUser
 var userType
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$database_interface.configureConnection("Data Source=SILVER;Initial Catalog=Bakery Database;Integrated Security=True;TrustServerCertificate=True")
+	$database_interface.configureConnection("Data Source=LAPTOP-DG7JVPNB\\SQLEXPRESS;Initial Catalog=Bakery Database;Integrated Security=True;TrustServerCertificate=True")
 	#$database_interface.handleQuery("SELECT B.ProductID, ProductName, Quantity, Cost FROM   [BAKED PRODUCT] AS B JOIN [PRODUCT INVENTORY] AS I ON B.ProductID = I.ProductID")
 	#print (currentData)
 	await get_tree().process_frame
